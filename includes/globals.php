50d2452f329210484560c274e41ce7f1b58b4789
Seth Kerr <sethmsk88@yahoo.com>
2018-04-24 21:31:04 -0400

    Fix global app path variables

--- includes/globals.php
+++ includes/globals.php
@@ -1,6 +1,6 @@
 <?php
 	define("APP_NAME", "User Management System");
-	define("APP_PATH",  $_SERVER['DOCUMENT_ROOT'] . "/apps/user_management/");
-    define("APP_PATH_URL", "http://" . $_SERVER['HTTP_HOST'] . "/apps/user_management/");
+	define("APP_PATH",  $_SERVER['DOCUMENT_ROOT'] . "/bootstrap/apps/user_management/");
+    define("APP_PATH_URL", "http://" . $_SERVER['HTTP_HOST'] . "/bootstrap/apps/user_management/");
     define("APP_HOMEPAGE", "users");
 ?>
